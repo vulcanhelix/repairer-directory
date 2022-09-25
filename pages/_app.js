300154aab47333611000b590a9b074516d67d818
vulcanhelix <47136857+vulcanhelix@users.noreply.github.com>
2022-09-25 18:02:01 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import { SiteLayout } from '@/layout'
 import { defaultSEO } from '../next-seo.config'
 import { theme } from '../styles/theme'
 import '../styles/css/global.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
   const getLayout =
@@ -13,8 +14,23 @@ export default function App({ Component, pageProps }) {
 
   return (
     <ChakraProvider theme={theme}>
+       <Script
+          strategy="afterInteractive"
+        src={"https://www.googletagmanager.com/ns.html?id=GTM-NRSKND7"}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+    {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'GTM-NRSKND7');
+    `}
+  </Script>
       <DefaultSeo {...defaultSEO} />
       {getLayout(<Component {...pageProps} />)}
+     
     </ChakraProvider>
   )
 }
+
+
